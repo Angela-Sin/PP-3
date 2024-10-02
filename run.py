be071ea8c15688950149146026bc01ceaa6da7d3
Anzela Siniavskaja <v6zmaqkl@students.codeinstitute.net>
2024-10-02 20:25:40 +0000

    add intro and fixed bug, programe was confused as I used word 'start' instead 'play'

--- run.py
+++ run.py
@@ -2,10 +2,40 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-def intro():
-    print("You are Alice. You've fallen a rabbit hole into the strange and magical world of Wonderland")
-    print("You must find your way home, but first, you must explore this curious place and face its many challenges.")
+def play():
+    print("text text text")
+    print("rules rules rules")
     print("GOOD LUCK!")
+    print()
+    print("Where would you like to go?")
+    print()
+    print("1.")
+    print("2.")
+    print("3.")
+    
+
+firstChoice = input("which path you prefere? (1/2/3): ")
+if firstChoice == '1':
+    print()
+    choice1()
+elif firstChoice == '2':
+    print()
+    choice2()
+elif firstChoice == '3':
+    print()
+    choice3()
+
+
+def choice1():
+    print()
+
+
+def choice2():
+    print()
+
+
+def choice3():
+    print()
 
 
 print(""" ____ _____  _    ____ _____    ____    _    __  __ _____ 
@@ -18,4 +48,13 @@ startGame = input('Would You like to play? (Y/N): ')
 if startGame == 'N':
     print("Next time")
 elif startGame == 'Y':
-    intro()
+    play()
+
+
+
+
+
+
+
+
+
