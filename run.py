0609be89f1f6580a6a0c80f1949cfffae8a3b081
Anzela Siniavskaja <v6zmaqkl@students.codeinstitute.net>
2024-10-02 21:09:42 +0000

    cleaned up some code

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 def play():
     print("text text text")
@@ -14,29 +11,6 @@ def play():
     print("3.")
     
 
-firstChoice = input("which path you prefere? (1/2/3): ")
-if firstChoice == '1':
-    print()
-    choice1()
-elif firstChoice == '2':
-    print()
-    choice2()
-elif firstChoice == '3':
-    print()
-    choice3()
-
-
-def choice1():
-    print()
-
-
-def choice2():
-    print()
-
-
-def choice3():
-    print()
-
 
 print(""" ____ _____  _    ____ _____    ____    _    __  __ _____ 
 / ___|_   _|/ \  |  _ \_   _|  / ___|  / \  |  \/  | ____|
@@ -49,6 +23,18 @@ if startGame == 'N':
     print("Next time")
 elif startGame == 'Y':
     play()
+
+
+firstChoice = input("which path you prefere? (1/2/3): ")
+if firstChoice == '1':
+    print()
+    choice1()
+elif firstChoice == '2':
+    print()
+    choice2()
+elif firstChoice == '3':
+    print()
+    choice3()
 
 
 
