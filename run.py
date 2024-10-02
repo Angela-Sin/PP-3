2644298098df66712d6de49b4e0f8ff89a457e7b
Anzela Siniavskaja <v6zmaqkl@students.codeinstitute.net>
2024-10-02 19:12:16 +0000

    add title, startGame function and print template for rules

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def intro():
+    print("You are Alice. You've fallen a rabbit hole into the strange and magical world of Wonderland")
+    print("You must find your way home, but first, you must explore this curious place and face its many challenges.")
+    print("GOOD LUCK!")
+
+
+print(""" ____ _____  _    ____ _____    ____    _    __  __ _____ 
+/ ___|_   _|/ \  |  _ \_   _|  / ___|  / \  |  \/  | ____|
+\___ \ | | / _ \ | |_) || |   | |  _  / _ \ | |\/| |  _|  
+ ___) || |/ ___ \|  _ < | |   | |_| |/ ___ \| |  | | |___ 
+|____/ |_/_/   \_\_| \_\|_|    \____/_/   \_\_|  |_|_____|""")
+
+startGame = input('Would You like to play? (Y/N): ')
+if startGame == 'N':
+    print("Next time")
+elif startGame == 'Y':
+    intro()
