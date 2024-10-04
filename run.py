9206927b419cc9bad22349896f88667718c1cbc1
Anzela Siniavskaja <v6zmaqkl@students.codeinstitute.net>
2024-10-04 12:31:16 +0000

    add some decor and chance to start the game with small letter n/y

--- run.py
+++ run.py
@@ -9,6 +9,8 @@ def play():
     print("1. Follow the White Rabbit")
     print("2. Visit the Mad Hatter's Tea Party")
     print("3. Enter the Queen of Hearts' Castle")
+
+
     
 
 
@@ -22,23 +24,26 @@ print("""     _    _ _            _
   \ V  V / (_) | | | | (_| |  __/ | (_| | | | | (_| |
    \_/\_/ \___/|_| |_|\__,_|\___|_|\__,_|_| |_|\__,_|""")
 
+print()
+print()
+print("""   _   /  |
+       | \  \/_/
+       \_\| / __              
+          \/_/__\           .--='/~\
+   ____,__/__,_____,______)/   /{~}}}
+   -,-----,--\--,-----,---,\'-' {{~}}
+           __/\_            '--=.\}/
+          /_/ |\\
+               \/""")
+
 startGame = input('Would You like to play? (Y/N): ')
-if startGame == 'N':
+if startGame == 'N' or startGame == "n":
     print("Next time")
-elif startGame == 'Y':
+elif startGame == 'Y' or startGame == "y":
     play()
 
 
-firstChoice = input("which path you prefere? (1/2/3): ")
-if firstChoice == '1':
-    print()
-    choice1()
-elif firstChoice == '2':
-    print()
-    choice2()
-elif firstChoice == '3':
-    print()
-    choice3()
+
 
 
 
